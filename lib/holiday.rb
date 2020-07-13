7fe6afd370fabc80314bcffd4887c8a6bc917636
Amanda Walker <walker.akw@gmail.com>
2020-07-13 03:56:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,11 +71,11 @@ def all_holidays_with_bbq(holiday_supplies)
   # include the string "BBQ"
     bbq_holidays = []
     holidays_supplies.each do |seasons, data|
-      data.each {|holiday, supplies| 
+      data.each {|holiday, supplies|
         supplies.each do |i|
           if supplies.detect{|i| i == "BBQ"}}
           bbq_holidays << holiday
-        end #do sup.each  
+        end #do sup.each
     end #do hol_sup
     return bbq_holidays
 end
