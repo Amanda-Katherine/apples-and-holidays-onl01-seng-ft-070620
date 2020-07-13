bf71808896041f7ab4375e8b43d9df70ff0fb93c
Amanda Walker <walker.akw@gmail.com>
2020-07-13 02:19:18 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    puts season.to_s
+    puts season.capitalize.to_s
     binding.pry
       holiday.values.each {|supplies| puts supplies}
 
